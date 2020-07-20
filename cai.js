55bc2c27a489156cd80e25d491e593b36151da0d
unknown <grantmatap@gmail.com>
2020-07-19 21:21:06 -0500

    version5

--- cai.js
+++ cai.js
@@ -490,9 +490,11 @@
     }
 
     function createGuid() {
-        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+        //return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+        return "xxxx".replace(/[xy]/g, c => {
             let r = Math.random() * 16 | 0,
                 v = c === "x" ? r : (r & 0x3 | 0x8);
+            //return v.toString(16);
             return v.toString(16);
         });
     }
