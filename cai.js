2b87541f0d9b68b5aad30706a23bf8b1bf42284d
unknown <grantmatap@gmail.com>
2020-07-20 09:01:23 -0500

    version7

--- cai.js
+++ cai.js
@@ -490,11 +490,10 @@
     }
 
     function createGuid() {
-        //return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {//
-        return "xxxx".replace(/[xy]/g, c => {
+        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {        
             let r = Math.random() * 16 | 0,
-                v = c === "x" ? r : (r & 0x3 | 0x8);
-            //return v.toString(16);
+                v = c === "x" ? r : (r & 0x3 | 0x8);            
+                console.log("guild:"+v);
             return v.toString(16);
         });
     }
